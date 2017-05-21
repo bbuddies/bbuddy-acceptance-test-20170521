901055e213c84318716001cb66e65f89f9f2c76e
Jackson Zhang <zbcjackson@gmail.com>
2017-05-21 14:46:46 +0800

    Implement budgets list assertion stop

--- features/step_definitions/budgets_steps.rb
+++ features/step_definitions/budgets_steps.rb
@@ -1,12 +1,13 @@
-When(/^add budget as month (\d+-\d+) and amount (\d+)$/) do |month, amount|
+When(/^add budget as (month \d+-\d+ and amount \d+)$/) do |budget|
   touch 'Budgets'
   touch 'Add'
-  clear_then_enter_text 'month', month
-  clear_then_enter_text 'amount', amount
+  clear_then_enter_text 'month', budget.month
+  clear_then_enter_text 'amount', budget.amount
   touch 'Save'
 end
 
-Then(/^you will see all budgets as below$/) do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^you will see all budgets as below$/) do |budgets|
+  budget = budgets[0]
+  wait_for_text budget.month
+  wait_for_text budget.amount
 end
