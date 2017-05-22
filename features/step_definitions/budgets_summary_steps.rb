7f19c5dbeff066c5c92d21c1a3941b6de6af2bd9
LittleLin <walle.kobayashi@gmail.com>
2017-05-22 17:57:28 +0800

    fix acceptance test bugs

--- features/step_definitions/budgets_summary_steps.rb
+++ features/step_definitions/budgets_summary_steps.rb
@@ -1,9 +1,9 @@
-When(/^query date as (startDate \d+-\d+-\d+ and endDate \d+-\d+-\d+)$/) do |queryDate|
-  touch 'BudgetSummary'
+When(/^query date as startDate (\d+-\d+-\d+) and endDate (\d+-\d+-\d+)$/) do |startDate, endDate|
+  touch 'Budgets'
   touch 'Query'
-  clear_then_enter_text 'startDate', queryDate.startDate
-  clear_then_enter_text 'endDate', queryDate.endDate
-  touch 'Seach'
+  clear_then_enter_text 'startDate', startDate
+  clear_then_enter_text 'endDate', endDate
+  touch 'Query'
 end
 
 Then(/^you will get summary amount is (\d+)$/) do |amount|
